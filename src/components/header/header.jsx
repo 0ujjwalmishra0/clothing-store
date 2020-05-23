2f8cb12e3e7f04776e57950f258c8435adcfa45d
0ujjwalmishra0 <ujjwalcoolguy05@gmail.com>
2020-05-23 06:57:12 +0530

    added cart dropdown

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,10 +3,12 @@ import "./header.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase-util";
+import CartIcon from '../cart-icon/cart-icon';
 
 import {connect} from 'react-redux';
+import CartDropdown from "../cart-dropdown/cart-dropdown";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser,hidden,photoURL }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -26,18 +28,33 @@ const Header = ({ currentUser }) => (
           <Link className="option" to="/signin">SIGN IN
           </Link>
         )}
+        <CartIcon />
      
-      <img className= 'image' alt= 'profile'
+      
+        <img className= 'image' alt= 'profile'
       src= 
       'https://lh3.googleusercontent.com/a-/AOh14GhXRGpO2oMbGX0vANrhYOzwwVtJ5I1PWVtIOiNZ'
       />
-      {console.log(currentUser)};
+      
+      {console.log(currentUser)}
+      {/* {console.log(currentUser.photoURL)} */}
+      
     </div>
+     
+   { hidden ? null :<CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state)=> ({
-  currentUser: state.user.currentUser
-});
+// from state we want user , and from user we want current user
 
+// const mapStateToProps = (state)=> ({
+//   currentUser: state.user.currentUser
+// });
+
+const mapStateToProps = ({user: {currentUser},cart: {hidden},})=> ({
+  currentUser,
+  hidden,
+  // photoURL: currentUser.photoURL,
+  //now pull hidden value of
+});
 export default connect (mapStateToProps)(Header);
