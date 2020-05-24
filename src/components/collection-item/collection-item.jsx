e351390f48713d91a1f02d755816db30e0f53008
0ujjwalmishra0 <ujjwalcoolguy05@gmail.com>
2020-05-24 17:37:52 +0530

    added "add to cart" button in shop page

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./collection-item.scss";
+import CustomButton from "../custom-button/custom-button";
+
+
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
@@ -13,6 +16,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name"> {name}</span>
       <span className="price"> {price}</span>
     </div>
+    <CustomButton inverted > Add to Cart</CustomButton>
   </div>
 );
 
--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import "./custom-button.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, inverted , ...otherProps }) => (
   <button
-    className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    // className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    // {...otherProps}
+    
+    className={`${inverted ? "inverted" : ""} ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
     {...otherProps}
   >
     {/* {console.log(otherProps)} */}
