ae1a4d3cb243944d1641f83ca124ac12b0c737b0
0ujjwalmishra0 <ujjwalcoolguy05@gmail.com>
2020-05-24 23:27:18 +0530

    added cart.actions in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -5,3 +5,10 @@ export const toggleCartHidden = ()=> ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 
 });
+
+
+// get item and return new action type object
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item,
+});
